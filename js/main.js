5b8973f71a4293838a6640105def40543b513238
Leonardo Tricotti <tricottileo@gmail.com>
2022-07-25 10:20:59 -0300

    primer commit proyecto final

---
+++ js/main.js
@@ -0,0 +1,90 @@
+//Saldo inicial
+let saldo = 100000;
+let dinero = new Intl.NumberFormat("es-AR", {
+  style: "currency",
+  currency: "ARS",
+}).format(saldo);
+
+//Menu inicio
+let seleccion = prompt(
+  "Seleccione la operación deseada: \n1) Consulta de saldo  \n2) Depósitos \n3) Extracciones \n4) Salir"
+);
+
+function operar(){
+  return seleccion;
+}
+
+let menu = operar(); 
+
+//Funcion nueva operacion
+function continuar() {
+  let continuo = prompt("Desea resalizar otra operacion? S/N: ");
+  if (continuo == "S" || continuo == "s") {
+    menu
+  } else if (continuo == "n" || continuo == "N") {
+    seleccion = "4";
+  } else {
+    alert("Elija una opción valida");
+    operar();
+  }
+}
+
+//Función para consultar el saldo
+const consultar = () => {
+  return alert("Su saldo es: " + dinero);
+};
+
+//Función para realizar depositos
+function depositar() {
+  let deposito = parseFloat(prompt("Ingrese el monto que desea depósitar: "));
+  saldo = saldo + deposito;
+  dinero = new Intl.NumberFormat("es-AR", {
+    style: "currency",
+    currency: "ARS",
+  }).format(saldo);
+  return alert("Operación realizada con exíto. \nSu saldo es: " + dinero);
+}
+
+//Función para realizar extracciones
+function extraer() {
+  let extraccion = parseInt(prompt("Ingrese el monto que desea extraer: "));
+  saldo = saldo - extraccion;
+  dinero = new Intl.NumberFormat("es-AR", {
+    style: "currency",
+    currency: "ARS",
+  }).format(saldo);
+  return alert("Operación realizada con exíto. \nSu saldo es: " + dinero);
+}
+
+//Función para salir del sistema
+function salir() {
+  return alert("Gracias por utilizar nuestros servicios.");
+}
+
+//Programa principal
+while (seleccion != "4") {
+  switch (seleccion) {
+    case "1":
+      consultar();
+      break;
+
+    case "2":
+      depositar();
+      break;
+
+    case "3":
+      extraer();
+      break;
+
+    case "continuo":
+      continuar();
+      break;
+
+    default:
+      alert("Opcion inválida. Vuelva a intentarlo.");
+      break;
+  }
+  continuar();
+}
+
+salir();
